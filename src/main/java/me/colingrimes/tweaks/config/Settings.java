bced77c00eea52177d72734fa1df29c1b892565f
Scill <realscill@gmail.com>
2025-03-30 10:03:14 -0400

    Add horse statistics tweak

--- src/main/java/me/colingrimes/tweaks/config/Settings.java
+++ src/main/java/me/colingrimes/tweaks/config/Settings.java
@@ -2,7 +2,9 @@ package me.colingrimes.tweaks.config;
 
 import me.colingrimes.midnight.config.annotation.Configuration;
 import me.colingrimes.midnight.config.option.Option;
+import me.colingrimes.midnight.message.Message;
 
+import static me.colingrimes.midnight.config.option.OptionFactory.message;
 import static me.colingrimes.midnight.config.option.OptionFactory.option;
 
 @Configuration
@@ -15,6 +17,8 @@ public interface Settings {
 	Option<Boolean> TWEAK_DROP_MAGNET = option("tweaks.drop-magnet", true);
 	Option<Boolean> TWEAK_ENTITY_EQUIP = option("tweaks.entity-equip", true);
 	Option<Boolean> TWEAK_HAY_BALE_BREAD = option("tweaks.hay-bale-bread", false);
+	Option<Boolean> TWEAK_HORSE_STATISTICS = option("tweaks.horse-statistics.toggle", false);
+	     Message<?> TWEAK_HORSE_STATISTICS_MESSAGE = message("tweaks.horse-statistics.message");
 	Option<Boolean> TWEAK_INVENTORY_CRAFTING = option("tweaks.inventory-crafting", true);
 	Option<Boolean> TWEAK_SPONGE_DRY_FIRE = option("tweaks.sponge-dry-fire", true);
 	Option<Boolean> TWEAK_NAME_TAG_DYE = option("tweaks.name-tag-dye", true);
