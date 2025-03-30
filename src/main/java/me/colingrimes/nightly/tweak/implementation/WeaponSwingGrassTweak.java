7972d47ee5b5a4676e995fbc62cf55231c8d803e
Scill <realscill@gmail.com>
2025-03-29 20:59:15 -0400

    Add the XP fill and weapon swing grass tweaks

---
+++ src/main/java/me/colingrimes/nightly/tweak/implementation/WeaponSwingGrassTweak.java
@@ -0,0 +1,55 @@
+package me.colingrimes.nightly.tweak.implementation;
+
+import me.colingrimes.nightly.Nightly;
+import me.colingrimes.nightly.config.Settings;
+import me.colingrimes.nightly.tweak.Tweak;
+import org.bukkit.FluidCollisionMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.util.RayTraceResult;
+
+import javax.annotation.Nonnull;
+
+public class WeaponSwingGrassTweak extends Tweak {
+
+	public WeaponSwingGrassTweak(@Nonnull Nightly plugin) {
+		super(plugin, "weapon_swing_grass");
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return Settings.TWEAK_WEAPON_SWING_GRASS.get();
+	}
+
+	@EventHandler
+	public void onPlayerInteract(@Nonnull PlayerInteractEvent event) {
+		if (event.getHand() != EquipmentSlot.HAND || event.getAction() != Action.LEFT_CLICK_BLOCK) {
+			return;
+		}
+
+		Block block = event.getClickedBlock();
+		if (block == null || !block.isPassable()) {
+			return;
+		}
+
+		Player player = event.getPlayer();
+		Material type = player.getInventory().getItemInMainHand().getType();
+		if (!type.name().endsWith("_SWORD") && !type.name().endsWith("_AXE")) {
+			return;
+		}
+
+		Location eye = player.getEyeLocation();
+		RayTraceResult result = player.getWorld().rayTrace(eye, eye.getDirection(), 3.5, FluidCollisionMode.NEVER, true, 0, e -> !e.equals(player));
+		if (result != null && result.getHitEntity() != null) {
+			event.getPlayer().attack(result.getHitEntity());
+		}
+
+		event.setCancelled(true);
+	}
+}
---
+++ src/main/java/me/colingrimes/nightly/tweak/implementation/XpFillTweak.java
@@ -0,0 +1,50 @@
+package me.colingrimes.nightly.tweak.implementation;
+
+import me.colingrimes.midnight.util.bukkit.Experience;
+import me.colingrimes.midnight.util.bukkit.Inventories;
+import me.colingrimes.midnight.util.bukkit.Items;
+import me.colingrimes.nightly.Nightly;
+import me.colingrimes.nightly.config.Settings;
+import me.colingrimes.nightly.tweak.Tweak;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import javax.annotation.Nonnull;
+
+public class XpFillTweak extends Tweak {
+
+	public XpFillTweak(@Nonnull Nightly plugin) {
+		super(plugin, "xp_fill");
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return Settings.TWEAK_XP_FILL.get();
+	}
+
+	@EventHandler
+	public void onPlayerInteract(@Nonnull PlayerInteractEvent event) {
+		if (event.getHand() != EquipmentSlot.HAND || event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getClickedBlock() == null) {
+			return;
+		}
+
+		Player player = event.getPlayer();
+		PlayerInventory inventory = player.getInventory();
+		ItemStack item = inventory.getItemInMainHand();
+		if (item.getType() != Material.GLASS_BOTTLE || event.getClickedBlock().getType() != Material.ENCHANTING_TABLE) {
+			return;
+		}
+
+		if (Experience.fromPlayer(player) >= Settings.TWEAK_XP_FILL_COST.get() && Inventories.removeSingle(inventory, item) == 1) {
+			Experience.remove(player, Settings.TWEAK_XP_FILL_COST.get());
+			Inventories.give(player, Items.of(Material.EXPERIENCE_BOTTLE).build(), true);
+			event.setCancelled(true);
+		}
+	}
+}
