671d7bbe0b216ed89d4fe8ffe21e86062851a005
Scill <realscill@gmail.com>
2025-05-05 17:44:46 -0400

    Refactor config logic

--- src/main/java/me/colingrimes/tweaks/config/Settings.java
+++ src/main/java/me/colingrimes/tweaks/config/Settings.java
@@ -1,52 +1,100 @@
 package me.colingrimes.tweaks.config;
 
-import me.colingrimes.midnight.config.annotation.Configuration;
-import me.colingrimes.midnight.config.option.Option;
-import me.colingrimes.midnight.message.Message;
+import me.colingrimes.tweaks.Tweaks;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 
-import java.util.Objects;
-import java.util.Set;
+import javax.annotation.Nonnull;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static me.colingrimes.midnight.config.option.OptionFactory.message;
-import static me.colingrimes.midnight.config.option.OptionFactory.option;
+public class Settings {
 
-@Configuration
-public interface Settings {
+	private final List<Option<?>> options = new ArrayList<>();
+	public final Option<Boolean>       TWEAK_ANVIL_COLOR                = option("tweaks.anvil-color", true);
+	public final Option<Boolean>       TWEAK_ANVIL_REPAIR               = option("tweaks.anvil-repair", true);
+	public final Option<Boolean>       TWEAK_CROPS_BONE_MEAL            = option("tweaks.crops-bone-meal", true);
+	public final Option<Boolean>       TWEAK_CROPS_TRAMPLE_PROOF        = option("tweaks.crops-trample-proof", true);
+	public final Option<Boolean>       TWEAK_DOUBLE_DOORS               = option("tweaks.double-doors.toggle", true);
+	public final Option<Boolean>       TWEAK_DOUBLE_DOORS_IRON_DOORS    = option("tweaks.double-doors.iron-doors", false);
+	public final Option<Boolean>       TWEAK_DROPS_MAGNET               = option("tweaks.drops-magnet", true);
+	public final Option<Boolean>       TWEAK_ENTITY_EQUIP               = option("tweaks.entity-equip", false);
+	public final Option<Boolean>       TWEAK_ENTITY_SET_ON_FIRE         = option("tweaks.entity-set-on-fire", true);
+	public final Option<Boolean>       TWEAK_FIRE_DRIES_SPONGES         = option("tweaks.fire-dries-sponges", true);
+	public final Option<Boolean>       TWEAK_GLASS_BREAK                = option("tweaks.glass-break.toggle", true);
+	public final Option<Set<Material>> TWEAK_GLASS_BREAK_MATERIALS      = option("tweaks.glass-break", sec -> sec.getStringList("materials").stream().map(m -> Material.getMaterial(m.toUpperCase())).filter(Objects::nonNull).collect(Collectors.toSet()));
+	public final Option<Boolean>       TWEAK_HAY_BALE_BREAD             = option("tweaks.hay-bale-bread", false);
+	public final Option<Boolean>       TWEAK_HORSE_STATISTICS           = option("tweaks.horse-statistics.toggle", true);
+	public final Option<List<String>>  TWEAK_HORSE_STATISTICS_MESSAGE   = option("tweaks.horse-statistics.message", List.of());
+	public final Option<Boolean>       TWEAK_INVENTORY_CRAFTING         = option("tweaks.inventory-crafting", true);
+	public final Option<Boolean>       TWEAK_NAME_TAG_DYE               = option("tweaks.name-tag-dye", true);
+	public final Option<Boolean>       TWEAK_RECIPE_UNLOCK_ALL          = option("tweaks.recipe-unlock-all", true);
+	public final Option<Boolean>       TWEAK_PORTAL_EXPLOSION_PROOF     = option("tweaks.portal-explosion-proof", true);
+	public final Option<Boolean>       TWEAK_TORCH_THROW                = option("tweaks.torch-throw", true);
+	public final Option<Boolean>       TWEAK_VEHICLE_PICKUP             = option("tweaks.vehicle-pickup", true);
+	public final Option<Boolean>       TWEAK_WATER_BOTTLE_CONVERT_LAVA  = option("tweaks.water-bottle-convert-lava", true);
+	public final Option<Boolean>       TWEAK_WATER_BOTTLE_CRAFT         = option("tweaks.water-bottle-craft.toggle", true);
+	public final Option<Integer>       TWEAK_WATER_BOTTLE_CRAFT_AMOUNT  = option("tweaks.water-bottle-craft.amount", 8);
+	public final Option<Boolean>       TWEAK_WEAPON_SWING_THROUGH_GRASS = option("tweaks.weapon-swing-through-grass", true);
+	public final Option<Boolean>       TWEAK_XP_FILL                    = option("tweaks.xp-fill.toggle", true);
+	public final Option<Integer>       TWEAK_XP_FILL_COST               = option("tweaks.xp-fill.cost", 8);
+	public final Option<String>  RELOADED       = option("admin.reloaded", "&2&l✓ &a&lTweaks &ahas been reloaded. Registered &l{amount} &atweaks.");
+	public final Option<String>  RESET_RECIPES  = option("reset-recipes", "&2&l✓ &aRecipes have been reset for all online players.");
+	public final Option<Boolean> ENABLE_METRICS = option("enable-metrics", true);
 
-	Option<Boolean>       TWEAK_ANVIL_COLOR                = option("tweaks.anvil-color", true);
-	Option<Boolean>       TWEAK_ANVIL_REPAIR               = option("tweaks.anvil-repair", true);
-	Option<Boolean>       TWEAK_CROPS_BONE_MEAL            = option("tweaks.crops-bone-meal", true);
-	Option<Boolean>       TWEAK_CROPS_TRAMPLE_PROOF        = option("tweaks.crops-trample-proof", true);
-	Option<Boolean>       TWEAK_DOUBLE_DOORS               = option("tweaks.double-doors.toggle", true);
-	Option<Boolean>       TWEAK_DOUBLE_DOORS_IRON_DOORS    = option("tweaks.double-doors.iron-doors", false);
-	Option<Boolean>       TWEAK_DROPS_MAGNET               = option("tweaks.drops-magnet", true);
-	Option<Boolean>       TWEAK_ENTITY_EQUIP               = option("tweaks.entity-equip", false);
-	Option<Boolean>       TWEAK_ENTITY_SET_ON_FIRE         = option("tweaks.entity-set-on-fire", true);
-	Option<Boolean>       TWEAK_FIRE_DRIES_SPONGES         = option("tweaks.fire-dries-sponges", true);
-	Option<Boolean>       TWEAK_GLASS_BREAK                = option("tweaks.glass-break.toggle", true);
-	Option<Set<Material>> TWEAK_GLASS_BREAK_MATERIALS      = option("tweaks.glass-break", sec -> sec.getStringList("materials").stream().map(m -> Material.getMaterial(m.toUpperCase())).filter(Objects::nonNull).collect(Collectors.toSet()));
-	Option<Boolean>       TWEAK_HAY_BALE_BREAD             = option("tweaks.hay-bale-bread", false);
-	Option<Boolean>       TWEAK_HORSE_STATISTICS           = option("tweaks.horse-statistics.toggle", true);
-	     Message<?>       TWEAK_HORSE_STATISTICS_MESSAGE  = message("tweaks.horse-statistics.message");
-	Option<Boolean>       TWEAK_INVENTORY_CRAFTING         = option("tweaks.inventory-crafting", true);
-	Option<Boolean>       TWEAK_NAME_TAG_DYE               = option("tweaks.name-tag-dye", true);
-	Option<Boolean>       TWEAK_RECIPE_UNLOCK_ALL          = option("tweaks.recipe-unlock-all", true);
-	Option<Boolean>       TWEAK_PORTAL_EXPLOSION_PROOF     = option("tweaks.portal-explosion-proof", true);
-	Option<Boolean>       TWEAK_TORCH_THROW                = option("tweaks.torch-throw", true);
-	Option<Boolean>       TWEAK_VEHICLE_PICKUP             = option("tweaks.vehicle-pickup", true);
-	Option<Boolean>       TWEAK_WATER_BOTTLE_CONVERT_LAVA  = option("tweaks.water-bottle-convert-lava", true);
-	Option<Boolean>       TWEAK_WATER_BOTTLE_CRAFT         = option("tweaks.water-bottle-craft.toggle", true);
-	Option<Integer>       TWEAK_WATER_BOTTLE_CRAFT_AMOUNT  = option("tweaks.water-bottle-craft.amount", 8);
-	Option<Boolean>       TWEAK_WEAPON_SWING_THROUGH_GRASS = option("tweaks.weapon-swing-through-grass", true);
-	Option<Boolean>       TWEAK_XP_FILL                    = option("tweaks.xp-fill.toggle", true);
-	Option<Integer>       TWEAK_XP_FILL_COST               = option("tweaks.xp-fill.cost", 8);
+	private final Tweaks plugin;
 
-	// Admin stuff.
-	Message<?> RELOADED      = message("admin.reloaded", "&2&l✓ &a&lTweaks &ahas been reloaded. Registered &l{amount} &atweaks.");
-	Message<?> RESET_RECIPES = message("reset-recipes", "&2&l✓ &aRecipes have been reset for all online players.");
+	public Settings(@Nonnull Tweaks plugin) {
+		this.plugin = plugin;
+		plugin.saveDefaultConfig();
+	}
 
-	// Misc stuff.
-	Option<Boolean> ENABLE_METRICS = option("enable-metrics", true);
+	/**
+	 * Reloads the settings, updating the values of all options.
+	 */
+	public void reload() {
+		plugin.reloadConfig();
+		FileConfiguration config = plugin.getConfig();
+		options.forEach(option -> option.reload(config));
+	}
+
+	@Nonnull
+	private Option<String> option(@Nonnull String path, @Nonnull String def) {
+		Option<String> option = new Option<>(config -> config.getString(path, def));
+		options.add(option);
+		return option;
+	}
+
+	@Nonnull
+	private Option<List<String>> option(@Nonnull String path, @Nonnull List<String> def) {
+		Option<List<String>> option = new Option<>(config -> {
+			List<String> value = config.getStringList(path);
+			return value.isEmpty() ? def : value;
+		});
+		options.add(option);
+		return option;
+	}
+
+	@Nonnull
+	private Option<Integer> option(@Nonnull String path, int def) {
+		Option<Integer> option = new Option<>(config -> config.getInt(path, def));
+		options.add(option);
+		return option;
+	}
+
+	@Nonnull
+	private Option<Boolean> option(@Nonnull String path, boolean def) {
+		Option<Boolean> option = new Option<>(config -> config.getBoolean(path, def));
+		options.add(option);
+		return option;
+	}
+
+	@Nonnull
+	private <T> Option<T> option(@Nonnull String path, @Nonnull Function<ConfigurationSection, T> extractor) {
+		Option<T> option = new Option<>(config -> extractor.apply(config.getConfigurationSection(path)));
+		options.add(option);
+		return option;
+	}
 }
